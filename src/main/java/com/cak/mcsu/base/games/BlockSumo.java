d077a3cc726109cdc38038ade202517883ccc2ab
cakeGit <65340665+cakeGit@users.noreply.github.com>
2023-05-23 09:26:08 +0000

    (v0.1) Refactor finalise

--- src/main/java/com/cak/mcsu/base/games/BlockSumo.java
+++ src/main/java/com/cak/mcsu/base/games/BlockSumo.java
@@ -1,10 +1,11 @@
 package com.cak.mcsu.base.games;
 
 import com.cak.mcsu.base.inventories.BlockSumoInventory;
+import com.cak.mcsu.base.lang.BlockSumoLang;
 import com.cak.mcsu.base.loottables.BlockSumoLoot;
 import com.cak.mcsu.base.players.BlockSumoPlayer;
-import com.cak.mcsu.core.McsuPlayer;
 import com.cak.mcsu.base.texts.BlockSumoTexts;
+import com.cak.mcsu.core.McsuPlayer;
 import com.cak.mcsu.core.TimedEvent;
 import com.cak.mcsu.core.eventhandler.ActivityRule;
 import com.cak.mcsu.core.game.ActiveGame;
@@ -16,6 +17,8 @@ import com.cak.mcsu.core.scoreboard.PlayerScoreboard;
 import com.cak.mcsu.core.util.BukkitRunnableLater;
 import com.cak.mcsu.core.util.BukkitRunnableTimer;
 import com.cak.mcsu.core.util.LootTable;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.title.TitlePart;
 import org.bukkit.ChatColor;
@@ -25,177 +28,206 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
-import java.util.Arrays;
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class BlockSumo {
-  
+
   public BlockSumo() {
     super("blocksumo", "Block Sumo");
   }
-  
+
   static int maxPowerups = 5;
-  
+
   static boolean powerupsEnabled = false;
   static GamePlayerHelper<BlockSumoPlayer> playerHelper;
   static LootTable activeLootTable;
-  
+
   @Override
   public GameState createPrefixProvider() {
     return player -> {
       BlockSumoPlayer blockSumoPlayer = playerHelper.getPlayer(player);
-      return (blockSumoPlayer == null ? Component.text("") : blockSumoPlayer.getLivesTabString());
+      return (
+        blockSumoPlayer == null
+          ? Component.text("")
+          : blockSumoPlayer.getLivesTabString()
+      );
     };
   }
 
   @Override
   public GameState createActiveGameState() {
     return new GameState("Active")
-        .setOnEnable(() -> {
-          playerHelper.getPlayers().forEach(blockSumoPlayer ->
-              PlayerScoreboard.getScoreboard(blockSumoPlayer.getPlayer()).updateGamePrefix());
-          
-          powerupsEnabled = false;
-          new TimedEvent("Powerups I", 60, true)
-              .setOnEnd(() -> {
-                activeLootTable = BlockSumoLoot.powerupLootTableI;
-                powerupsEnabled = true;
-              });
-          
-          new TimedEvent("Powerups II", 60 + 300, true)
-              .setOnEnd(() -> {
-                activeLootTable = BlockSumoLoot.powerupLootTableII;
-              });
-          
-          new TimedEvent("Powerups III", 60 + 300 + 300, true)
-              .setOnEnd(() -> {
-                activeLootTable = BlockSumoLoot.powerupLootTableIII;
-              });
-          
-        })
-        .addGameFunctions(
-            playerHelper = new GamePlayerHelper<>(BlockSumoPlayer::new),
-            new GameInventoryHelper<>(BlockSumoInventory::new),
-            new TaskInterval(() -> {
-              for (BlockSumoPlayer player : playerHelper.getPlayers()) {
-                if (powerupsEnabled && player.hasPowerupsEnabled() && player.getLives() != 0
-                    && player.getLifePowerups() != maxPowerups) {
-                  player.getPlayer().toBukkit().getInventory().addItem(
-                      activeLootTable.generate()
-                  );
-                  player.incrementLifePowerups();
-                  player.getPlayer().toBukkit().sendMessage(ChatColor.BLUE + "You have received a powerup! [" +
-                      player.getLifePowerups() + "/" + maxPowerups + "]"
-                  );
-                }
-              }
-            }, 0L, 20L * 30),
-            new EventListener() {
-              @EventHandler
-              public void onPlayerDeath(PlayerDeathEvent event) {
-                Player player = event.getPlayer();
-                BlockSumoPlayer bsPlayer = playerHelper.getPlayer(McsuPlayer.fromBukkit(event.getPlayer()));
-                bsPlayer.removeLife();
-                bsPlayer.resetLifePowerups();
-                bsPlayer.setPowerupsEnabled(false);
-                
-                if (bsPlayer.getLives() == 0) return;
-                ActiveGame.eliminatePlayer(player);
-                
-                AtomicInteger respawnTimeLeft = new AtomicInteger(5);
-                
-                new BukkitRunnableTimer((timer) -> {
-                  
-                  respawnTimeLeft.getAndDecrement();
-                  if (respawnTimeLeft.get() == 0) {
-                    
-                    Arrays.stream(ActiveGame.getGameMap().getGameSpawns()).findFirst().orElse(null)
-                        .teleportPlayerTo(player, ActiveGame.getWorld());
-  
-                    player.setGameMode(GameMode.SURVIVAL);
-                    player.sendMessage(ChatColor.GREEN +""+ ChatColor.BOLD + "Respawned!");
-                    bsPlayer.setPowerupsEnabled(true);
-                    bsPlayer.setSpawnProtectionEnabled(true);
-                    
-                    new BukkitRunnableLater(() -> {
-                      player.sendMessage(ChatColor.AQUA + "Spawn protection disabled!");
-                      bsPlayer.setSpawnProtectionEnabled(false);
-                    }, 20*5);
-                    
-                    timer.cancel();
-                  } else {
-                    player.sendTitlePart(
-                        TitlePart.TITLE,
-                        Component.text("Respawning in " + respawnTimeLeft + "!")
-                    );
-                    player.sendTitlePart(
-                        TitlePart.SUBTITLE,
-                        BlockSumoTexts.livesLeft(bsPlayer.getLives())
-                    );
-                  }
-                  
-                },20L, 20L);
-                
+      .setOnEnable(() -> {
+        playerHelper
+          .getPlayers()
+          .forEach(blockSumoPlayer ->
+            PlayerScoreboard
+              .getScoreboard(blockSumoPlayer.getPlayer())
+              .updateGamePrefix()
+          );
+
+        powerupsEnabled = false;
+        new TimedEvent("Powerups I", 60, true)
+          .setOnEnd(() -> {
+            activeLootTable = BlockSumoLoot.powerupLootTableI;
+            powerupsEnabled = true;
+          });
+
+        new TimedEvent("Powerups II", 60 + 300, true)
+          .setOnEnd(() -> {
+            activeLootTable = BlockSumoLoot.powerupLootTableII;
+          });
+
+        new TimedEvent("Powerups III", 60 + 300 + 300, true)
+          .setOnEnd(() -> {
+            activeLootTable = BlockSumoLoot.powerupLootTableIII;
+          });
+      })
+      .addGameFunctions(
+        playerHelper = new GamePlayerHelper<>(BlockSumoPlayer::new),
+        new GameInventoryHelper<>(BlockSumoInventory::new),
+        new TaskInterval(
+          () -> {
+            for (BlockSumoPlayer player : playerHelper.getPlayers()) {
+              if (
+                powerupsEnabled &&
+                player.hasPowerupsEnabled() &&
+                player.getLives() != 0 &&
+                player.getLifePowerups() != maxPowerups
+              ) {
+                player.getPlayer().toBukkit().addItem(activeLootTable.generate());
+                player.incrementLifePowerups();
+                player.getPlayer().toBukkit().sendMessage(BlockSumoLang.recivedPowerup(player.getLifePowerups(), maxPowerups));
               }
             }
-       );
+          },
+          0L,
+          20L * 30
+        ),
+        new EventListener() {
+          @EventHandler
+          public void onPlayerDeath(PlayerDeathEvent event) {
+            Player player = event.getPlayer();
+            BlockSumoPlayer bsPlayer = playerHelper.getPlayer(
+              McsuPlayer.fromBukkit(event.getPlayer())
+            );
+            bsPlayer.removeLife();
+            bsPlayer.resetLifePowerups();
+            bsPlayer.setPowerupsEnabled(false);
+
+            if (bsPlayer.getLives() == 0) return;
+            ActiveGame.eliminatePlayer(player);
+
+            AtomicInteger respawnTimeLeft = new AtomicInteger(5);
+
+            new BukkitRunnableTimer(
+              timer -> {
+                respawnTimeLeft.getAndDecrement();
+                if (respawnTimeLeft.get() == 0) {
+                  Arrays
+                    .stream(ActiveGame.getGameMap().getGameSpawns())
+                    .findFirst()
+                    .orElse(null)
+                    .teleportPlayerTo(player, ActiveGame.getWorld());
+
+                  player.setGameMode(GameMode.SURVIVAL);
+                  player.sendMessage(
+                    ChatColor.GREEN + "" + ChatColor.BOLD + "Respawned!"
+                  );
+                  bsPlayer.setPowerupsEnabled(true);
+                  bsPlayer.setSpawnProtectionEnabled(true);
+
+                  new BukkitRunnableLater(
+                    () -> {
+                      player.sendMessage(
+                        ChatColor.AQUA + "Spawn protection disabled!"
+                      );
+                      bsPlayer.setSpawnProtectionEnabled(false);
+                    },
+                    20 * 5
+                  );
+
+                  timer.cancel();
+                } else {
+                  player.sendTitlePart(
+                    TitlePart.TITLE,
+                    Component.text("Respawning in " + respawnTimeLeft + "!")
+                  );
+                  player.sendTitlePart(
+                    TitlePart.SUBTITLE,
+                    BlockSumoTexts.livesLeft(bsPlayer.getLives())
+                  );
+                }
+              }, 20L, 20L
+            );
+          }
+        }
+      );
   }
-  
+
   @Override
   public List<GameState> createGameStates() {
-return List.of(new GameState("Base", true)
+    return List.of(
+      new GameState("Base", true)
         .setOnEnable(() -> {
           ActivityRule.setEnabled(
-              ActivityRule.TILE_BREAKING, true,
-              ActivityRule.TILE_PLACING, true,
-              ActivityRule.TILE_DROP, false,
-              ActivityRule.DISABLE_TRAPDOORS, true,
-              ActivityRule.AUTO_IGNITE_TNT, true
+            ActivityRule.TILE_BREAKING, true,
+            ActivityRule.TILE_PLACING, true,
+            ActivityRule.TILE_DROP, false,
+            ActivityRule.DISABLE_TRAPDOORS, true,
+            ActivityRule.AUTO_IGNITE_TNT, true
           );
-          
-          //Map GameData
-          double buildRange = ActiveGame.getMapGameData().getDouble("buildRange");
-          double[] buildOrigin = Arrays.stream(
-              ActiveGame.getMapGameData().getString("buildOrigin")
-                  .split(" ")
-          ).mapToDouble(Double::valueOf).toArray();
-          double buildMaxHeight = ActiveGame.getMapGameData().getDouble("buildMaxHeight");
-          double killHeight = ActiveGame.getMapGameData().getDouble("killHeight");
-          
-          //Tp players falling off to platform
+
+          // Map GameData
+          double buildRange = ActiveGame
+            .getMapGameData().getDouble("buildRange");
+          double[] buildOrigin = Arrays
+            .stream(
+              ActiveGame.getMapGameData().getString("buildOrigin").split(" ")
+            ).mapToDouble(Double::valueOf).toArray();
+          double buildMaxHeight = ActiveGame
+            .getMapGameData().getDouble("buildMaxHeight");
+          double killHeight = ActiveGame
+            .getMapGameData().getDouble("killHeight");
+
+          // Tp players falling off to platform
           HeightZone lobbyHeightZone = new HeightZone(killHeight);
-          lobbyHeightZone.setOnInside(player -> player.teleport(
-              ActiveGame.getGameMap().getLobbySpawns()[0]
-                  .toBukkitPosition(ActiveGame.getWorld())
-          ));
-          
-          game.getLobbyGameState()
-              .addGameFunctions(
-                  lobbyHeightZone
-              );
-          
-          //Setup active state for players falling off
+          lobbyHeightZone.setOnInside(player ->
+            player.teleport(
+              ActiveGame
+                .getGameMap()
+                .getLobbySpawns()[0].toBukkitPosition(ActiveGame.getWorld())
+            )
+          );
+
+          game.getLobbyGameState().addGameFunctions(lobbyHeightZone);
+
+          // Setup active state for players falling off
           HeightZone activeHeightZone = new HeightZone(killHeight);
           activeHeightZone.setOnInside(player -> player.damage(9999));
-          
-          BuildLimitBypass buildLimitBypass = placeEvent -> placeEvent.getBlock().getType() == Material.TNT;
-          
-          game.getActiveGameState()
-              .addGameFunctions(
-                  activeHeightZone,
-                  new BuildRange(buildOrigin, buildRange, buildLimitBypass),
-                  new BuildHeight(buildMaxHeight, buildLimitBypass),
-                  new TaskInterval(() -> {
-                    for (BlockSumoPlayer player : playerHelper.getPlayers()) {
-                      player.getPlayer().toBukkit().sendActionBar(player.getLivesPrefix());
-                    }
-                  }, 20L, 20L)
-              );
+
+          BuildLimitBypass buildLimitBypass = placeEvent ->
+            placeEvent.getBlock().getType() == Material.TNT;
+
+          game
+            .getActiveGameState()
+            .addGameFunctions(
+              activeHeightZone,
+              new BuildRange(buildOrigin, buildRange, buildLimitBypass),
+              new BuildHeight(buildMaxHeight, buildLimitBypass),
+              new TaskInterval(
+                () -> {
+                  for (BlockSumoPlayer player : playerHelper.getPlayers()) {
+                    player.getPlayer().toBukkit().sendActionBar(player.getLivesPrefix());
+                  }
+                }, 20L, 20L
+              )
+            );
         })
         .addGameFunctions(
-            new MapProtection(),
-            new BlockReplenishing(blockStack -> blockStack.getType().name().endsWith("_WOOL"))
+          new MapProtection(),
+          new BlockReplenishing(blockStack ->
+            blockStack.getType().name().endsWith("_WOOL")
+          )
         )
-});
-  
+    );
+  }
 }
